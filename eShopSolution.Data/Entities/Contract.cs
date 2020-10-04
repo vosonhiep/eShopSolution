ccbce5dc301179ddae9e6a4affb1b1f9efb66dca
Võ Hiệp <vosonhiepa@gmail.com>
2020-10-04 23:32:53 +0700

    add classes

---
+++ eShopSolution.Data/Entities/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Contract
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Message { set; get; }
+        public Status Status { set; get; }
+    }
+}
