fa027289d547c1f7cb9eeec12e03e640a962406f
Võ Hiệp <vosonhiepa@gmail.com>
2020-10-05 22:08:10 +0700

    add class for datatable
    lần 2

---
+++ eShopSolution.Data/Entities/ProductTranslation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class ProductTranslation
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public string Detail { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoAlias { get; set; }
+        public string LanguageId { get; set; }
+        public Product Product { get; set; }
+        public Language Language { get; set; }
+    }
+}
