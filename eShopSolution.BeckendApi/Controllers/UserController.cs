e509b4e7b36f1f8674856fe6b0f7258fd3553da2
Võ Sơn Hiệp <vosonhiepa@gmail.com>
2021-01-13 09:27:59 +0700

    Create Login and Register APIs

---
+++ eShopSolution.BeckendApi/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.BeckendApi.System.Users;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BeckendApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm]LoginRequest request)
+        { 
+            var resultToken = await _userService.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("USername or password is incorrect");
+            }
+            return Ok(new { token = resultToken });
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm ] RegisterRequest request)
+        {
+            var result = await _userService.Register(request);
+            if (!result)
+            {
+                return BadRequest("Register is unsuccessful");
+            }
+            return Ok();
+        }
+    }
+}
---
+++ eShopSolution.BeckendApi/System/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BeckendApi.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authencate(LoginRequest request);
+        Task<bool> Register(RegisterRequest request);
+    }
+}
